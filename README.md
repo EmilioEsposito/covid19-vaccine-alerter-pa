a03dd566d0501cc83b46e6337e5ca71d8a277275
Emilio Esposito <espo412@gmail.com>
2021-02-13 20:53:50 +0000

    update for Allegheny Health and Riteaid site changes

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # covid19-vaccine-alerter-pa
+Author: Emilio Esposito
 
-A simple python script that will send an SMS text when a vaccine appointment in Pennsylvania is available. 
+This is a simple python script that will send an SMS text when vaccine appointments in Allegheny County Pennsylvania (and other various PA locations) are available. 
 
-Currently, it scrapes the following sites to check for vaccine appointments:
+Currently, the script scrapes the following sites to check for vaccine appointments:
 
-* https://www.alleghenycounty.us/Health-Department/Resources/COVID-19/COVID-19-Vaccine-Information.aspx
-* https://sr.reportsonline.com/sr/riteaid/PS2021
+* [Allegheny County Health Department](https://www.alleghenycounty.us/Health-Department/Resources/COVID-19/COVID-19-Vaccine-Information.aspx)
+* [Armstrong Center for Medicine & Health](https://acmh.appointlet.com/)
+* [RiteAid](https://sr.reportsonline.com/sr/riteaid/PS2021)
 
-PRs are welcome if you want to improve it or add more sites to check. You can email me at emilio@serniacapital.com with a list of phone numbers you would like to add to the SMS recipient alert list (I hope to soon automate the addition of numbers via a Google Form).
+**Disclaimer: Please don't rely on this as your only source of trying to schedule a vaccine appointment. I'm trying to keep it as up to date as possible but the landscape is changing very quickly. Please still consult the PA state website regularly:**
 
-This script is currently running on AWS EC2 via a cron job that I can set to run every X minutes (currently every 60 minutes). The EC2 instance is owned by my rental business (Sernia Capital LLC), which can bear all hosting/SMS costs (Twilio is relatively cheap, we can handle over 100k messages before the cost would become material). 
+https://www.health.pa.gov/topics/disease/coronavirus/Vaccine/Pages/Vaccine.aspx#map
+
+Pull Requests are welcome if you want to improve it or add more sites to scrape. You can email me at emilio@serniacapital.com with a list of phone numbers you would like to add to the SMS recipient alert list (I hope to soon automate the addition of numbers via a Google Form).
+
+This script is currently running on AWS EC2 via a cron job that I can set to run every X minutes (currently every 5 minutes). The EC2 instance is owned by my rental business (Sernia Capital LLC), which can bear all hosting/SMS costs (Twilio is relatively cheap, we can handle over 100k messages before the cost would become material).
+
+
--- covid19-vaccine-alerter-pa.py
+++ covid19-vaccine-alerter-pa.py
@@ -1,16 +1,17 @@
 import os
 import sys
+import datetime
+import json
+from twilio.rest import Client
+import requests
 
 # Dynamically change the working dir depending on if I'm testing locally or if it's running in prod on AWS EC2
 if os.environ['USER']=='ec2-user': # Production running on AWS ec2
-    os.chdir('/home/ec2-user/sandbox')
+    os.chdir('/home/ec2-user/covid19-vaccine-alerter-pa')
 else:   # Dev running locally
     os.chdir('/Users/eesposito/sandbox')
     sys.path.append('/Users/eesposito/sandbox')
 
-from googleapiclient.discovery import build
-import datetime
-import gmail_api_example as gm
 
 # Import TWILIO account credentials from a twilio_creds.json file. The file contents should look like this:
 # {"TWILIO_AUTH":"XXXXXXXX",
@@ -27,6 +28,7 @@ with open('twilio_creds.json','r') as f:
 # ]
 with open('sms_recipients.json','r') as f: # TODO: we should have this file auto-populated based off of Google Form submissions
     sms_recipients = json.load(f)
+    print(sms_recipients)
 
 account_sid = creds['TWILIO_SID']
 auth_token = creds['TWILIO_AUTH']
@@ -34,7 +36,7 @@ client = Client(account_sid, auth_token)
 
 # define simple function that will send a SMS to each of the recipients in the sms_recipients list
 def send_sms(body='', recipients=[]):
-
+    print(f'Sending SMS: {msg}')
     for recipient in recipients:
         message = client.messages.create(
             messaging_service_sid=creds['MESSAGING_SID'],
@@ -43,44 +45,34 @@ def send_sms(body='', recipients=[]):
         )
         print(message.sid)
 
-# Vaccine checker simple Riteaid
-import requests
 
-url = 'https://sr.reportsonline.com/sr/riteaid/PS2021'
-resp = requests.get(url)
-no_vaccine_msg = 'There are currently no COVID vaccine appointments available. Please check back tomorrow as we continue to add availability.'
-waiting_page = 'Your estimated wait time is:'
-if no_vaccine_msg in resp.text:
-    print(no_vaccine_msg)
-elif waiting_page in resp.text:
-    print(waiting_page)
-else:
-    msg = f'From Emilio: Potential vaccine appt available at RiteAid: {url}'
-    send_sms(body=msg, recipients=sms_recipients)
-
-# Allegheny County health Heroku links
+# Site 1: Allegheny County Health Department
+print('Checking Allegheny County Health Department')
 url = 'https://www.alleghenycounty.us/Health-Department/Resources/COVID-19/COVID-19-Vaccine-Information.aspx'
 resp = requests.get(url)
 
 # check the site for new date references:
 new_dates = [
-'January 29', 'January 31', 'February 4', 'February 5', 'February 6', 'February 7', 'February 8', 'February 14',
-'February 15', 'February 16', 'February 17', 'February 18', 'February 19', 'February 20', 'February 21', 'February 22',
+# 'January 29', 'January 31', 'February 4', 'February 5', 'February 6', 'February 7', 'February 8', 'February 14',
+# 'February 15','February 16', 'February 17', 'February 18', 'February 19',
+'February 20', 'February 21', 'February 22',
 'February 23', 'February 24', 'February 25', 'February 26', 'February 27', 'February 28', 'February 29',
-'March 1 ', 'March 2 ', 'March 3 ', 'March 4 ', 'March 5 ', 'March 6 ', 'March 7 ', 'March 8 ',
-'March 9 ', 'March 10 ', 'March 11 ', 'March 12 ', 'March 13 ', 'March 14 ', 'March 15 ', 'March 16 ',
-'March 17 ', 'March 18 ', 'March 19 ', 'March 20 ', 'March 21 ', 'March 22 ', 'March 23 ', 'March 24 ',
-'March 25 ', 'March 26 ', 'March 27 ', 'March 28 ', 'March 29 ', 'March 30 ', 'March 31 '
+'March 1 ', 'March 2 ', 'March 1,', 'March 2,', 'March 1.', 'March 2.',
+'March 3', 'March 4', 'March 5', 'March 6', 'March 7', 'March 8', 'March 9',
+'March 10', 'March 11', 'March 12', 'March 13', 'March 14', 'March 15', 'March 16',
+'March 17', 'March 18', 'March 19', 'March 20', 'March 21', 'March 22', 'March 23', 'March 24',
+'March 25', 'March 26', 'March 27', 'March 28', 'March 29', 'March 30', 'March 31'
 ]
 
 for new_date in new_dates:
     msg = f'From Emilio: Potential vaccine available! Allegheny Health scheduling link for {new_date} was just added: {url}'
     if new_date in resp.text:
         send_sms(body=msg, recipients=sms_recipients)
-    else:
-        print('No vaccine appointment links added.')
+    # else:
+    #     print('No vaccine appointment links added.')
 
-# Armstrong
+# Site 2: Armstrong Center for Medicine & Health
+print('Checking Armstrong Center for Medicine & Health')
 url = 'https://api.appointlet.com/organizations/120454/scheduler'
 resp = requests.get(url)
 import re
@@ -89,4 +81,29 @@ if 'true' in x:
     url = 'https://acmh.appointlet.com/'
     msg = f'From Emilio: Armstrong Hospital COVID Vaccine link: {url}'
     print(msg)
-    tw.send_sms(body=msg, recipients=sms_recipients)
+    send_sms(body=msg, recipients=sms_recipients)
+
+# Site 3: Riteaid
+print('Checking Riteaid')
+url = 'https://sr.reportsonline.com/sr/riteaid/PS2021'
+resp = requests.get(url)
+no_vaccine_msg = 'There are currently no COVID vaccine appointments available. Please check back tomorrow as we continue to add availability.'
+waiting_page = 'Your estimated wait time is:'
+event_ended = 'The event has ended'
+if no_vaccine_msg in resp.text:
+    print('RiteAid '+no_vaccine_msg)
+elif waiting_page in resp.text:
+    print('RiteAid '+waiting_page)
+elif event_ended in resp.text:
+    print('RiteAid '+event_ended)
+else:
+    msg = f'From Emilio: Potential vaccine appt available at RiteAid: {url}'
+    send_sms(body=msg, recipients=sms_recipients)
+    # send_sms(body='Prior text was a false alarm.', recipients=sms_recipients)
+
+# Send Emilio a daily text so he knows the script is still running, even if there are no new alerts:
+now = datetime.datetime.now()
+print('Current Time on EC2:',now)
+msg = f'COVID Vaccine Checker still running. Nothing to report.'
+if now.hour==23:
+    send_sms(body=msg, recipients=['+14123703550'])
